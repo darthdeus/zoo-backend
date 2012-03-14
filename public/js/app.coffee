1cb8ee50dfdc91c84cecaebe02ca03ca3593ba4c
Jakub Arnold <darthdeus@gmail.com>
2012-03-14 19:44:20 +0100

    Added buttons to select some colors

--- public/js/app.coffee
+++ public/js/app.coffee
@@ -1,5 +1,11 @@
 jQuery ->
 
+  color = '#333'
+
+  ($ '.colorpick button').click ->
+    color = ($ this).attr('data-color')
+
+
   paper = Raphael 'paper', 940, 400
 
   map = paper.rect 0, 0, 940, 400
@@ -7,7 +13,8 @@ jQuery ->
   map.attr 'stroke', 'none'
   map.click (e) ->
     circle = paper.circle e.offsetX, e.offsetY, 10
-    circle.attr 'fill', '#333'
+    circle.attr 'fill', color
+    circle.attr 'stroke', 'none'
     console.log e.offsetX, e.offsetY
 
 
--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,11 @@
 (function() {
 
   jQuery(function() {
-    var map, paper;
+    var color, map, paper;
+    color = '#333';
+    ($('.colorpick button')).click(function() {
+      return color = ($(this)).attr('data-color');
+    });
     paper = Raphael('paper', 940, 400);
     map = paper.rect(0, 0, 940, 400);
     map.attr('fill', '#eee');
@@ -9,7 +13,8 @@
     return map.click(function(e) {
       var circle;
       circle = paper.circle(e.offsetX, e.offsetY, 10);
-      circle.attr('fill', '#333');
+      circle.attr('fill', color);
+      circle.attr('stroke', 'none');
       return console.log(e.offsetX, e.offsetY);
     });
   });
