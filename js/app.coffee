fe07591d9a35f03ac4551a820ec49b058d9b2ac8
Jakub Arnold <darthdeus@gmail.com>
2012-03-14 21:55:22 +0100

    Nodes are now drawn on both maps and can be dragged around by the circle, dragging by the text doesn't work yet

--- js/app.coffee
+++ js/app.coffee
@@ -18,34 +18,90 @@ jQuery ->
     ($ '#json-output').html(json)
     console.log nodes
 
+  window.nodes = nodes
 
-  paper = Raphael 'paper', 940, 400
 
-  map = paper.rect 0, 0, 940, 400
-  map.attr 'fill', '#eee'
-  map.attr 'stroke', 'none'
-  map.click (e) ->
-    start = ->
-      console.log 'drag started'
-      @ox = @attr "cx"
-      @oy = @attr "cy"
-      @animate { r: 15, opacity: .25 }, 200, ">"
+  class Node
 
-    move = (dx, dy) ->
-      @attr {cx: @ox + dx, cy: @oy + dy }
-      console.log 'moving by', dx, dy
+    count = 0
 
-    up = -> @animate { r: 10, opacity: 1 }, 200, ">"
+    constructor: (gps_paper, target_paper, color) ->
+      @id = count++
+      @gps_paper = gps_paper
+      @target_paper = target_paper
+      @color = color
 
-    nodes.push
-      x: e.offsetX
-      y: e.offsetY
-      color: color
+    coords: ->
+      @circle
 
-    circle = paper.circle e.offsetX, e.offsetY, 10
-    circle.attr 'fill', color
-    circle.attr 'stroke', 'none'
-    circle.drag(move, start, up)
-    console.log e.offsetX, e.offsetY
+    draw_both: (x, y) ->
+      @draw_gps(x, y)
+      @draw_target(x, y)
+
+    draw_gps: (x, y) ->
+      @draw(x, y, @gps_paper)
+
+    draw_target: (x, y) ->
+      @draw(x, y, @target_paper)
+
+    draw: (x, y, paper) ->
+      @x = x
+      @y = y
+      @circle = paper.circle @x, @y, 10
+      @circle.attr 'fill', @color
+      @circle.attr 'stroke', 'none'
+
+      @text = paper.text @x, @y, @id
+      @text.attr 'fill', 'white'
+      window.text = @text
+
+      start = ->
+        @ox = @attr "cx"
+        @oy = @attr "cy"
+        @animate { r: 15, opacity: .25 }, 200, ">"
+      up = -> @animate { r: 10, opacity: 1 }, 200, ">"
+
+      # closure for the move function
+      text = @text
+
+      move = (dx, dy) ->
+        @attr {cx: @ox + dx, cy: @oy + dy }
+        text.attr {x: @ox + dx, y: @oy + dy }
+
+
+      @circle.drag move, start, up
+
+
+
+  gps_paper = Raphael 'gps_map', 940, 400
+
+  gps_map = gps_paper.rect 0, 0, 940, 400
+  gps_map.attr 'fill', '#eee'
+  gps_map.attr 'stroke', 'none'
+  gps_map.click (e) ->
+    node = new Node(gps_paper, target_paper, color)
+    node.draw_both(e.offsetX, e.offsetY)
+    nodes.push node
+
+
+  target_paper = Raphael 'target_map', 940, 400
+
+  target_map = target_paper.rect 0, 0, 940, 400
+  target_map.attr 'fill', '#eee'
+  target_map.attr 'stroke', 'none'
+  target_map.click (e) ->
+    node = new Node(gps_paper, target_paper, color)
+    node.draw_both(e.offsetX, e.offsetY)
+    nodes.push node
+    # nodes.push
+    #   x: e.offsetX
+    #   y: e.offsetY
+    #   color: color
+
+    # circle = paper.circle e.offsetX, e.offsetY, 10
+    # circle.attr 'fill', color
+    # circle.attr 'stroke', 'none'
+    # circle.drag(move, start, up)
+    # console.log e.offsetX, e.offsetY
 
 
--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 (function() {
 
   jQuery(function() {
-    var color, map, nodes, paper;
+    var Node, color, gps_map, gps_paper, nodes, target_map, target_paper;
     color = '#333';
     nodes = [];
     ($('.colorpick button')).click(function() {
@@ -19,44 +19,96 @@
       ($('#json-output')).html(json);
       return console.log(nodes);
     });
-    paper = Raphael('paper', 940, 400);
-    map = paper.rect(0, 0, 940, 400);
-    map.attr('fill', '#eee');
-    map.attr('stroke', 'none');
-    return map.click(function(e) {
-      var circle, move, start, up;
-      start = function() {
-        console.log('drag started');
-        this.ox = this.attr("cx");
-        this.oy = this.attr("cy");
-        return this.animate({
-          r: 15,
-          opacity: .25
-        }, 200, ">");
+    window.nodes = nodes;
+    Node = (function() {
+      var count;
+
+      count = 0;
+
+      function Node(gps_paper, target_paper, color) {
+        this.id = count++;
+        this.gps_paper = gps_paper;
+        this.target_paper = target_paper;
+        this.color = color;
+      }
+
+      Node.prototype.coords = function() {
+        return this.circle;
       };
-      move = function(dx, dy) {
-        this.attr({
-          cx: this.ox + dx,
-          cy: this.oy + dy
-        });
-        return console.log('moving by', dx, dy);
+
+      Node.prototype.draw_both = function(x, y) {
+        this.draw_gps(x, y);
+        return this.draw_target(x, y);
       };
-      up = function() {
-        return this.animate({
-          r: 10,
-          opacity: 1
-        }, 200, ">");
+
+      Node.prototype.draw_gps = function(x, y) {
+        return this.draw(x, y, this.gps_paper);
       };
-      nodes.push({
-        x: e.offsetX,
-        y: e.offsetY,
-        color: color
-      });
-      circle = paper.circle(e.offsetX, e.offsetY, 10);
-      circle.attr('fill', color);
-      circle.attr('stroke', 'none');
-      circle.drag(move, start, up);
-      return console.log(e.offsetX, e.offsetY);
+
+      Node.prototype.draw_target = function(x, y) {
+        return this.draw(x, y, this.target_paper);
+      };
+
+      Node.prototype.draw = function(x, y, paper) {
+        var move, start, text, up;
+        this.x = x;
+        this.y = y;
+        this.circle = paper.circle(this.x, this.y, 10);
+        this.circle.attr('fill', this.color);
+        this.circle.attr('stroke', 'none');
+        this.text = paper.text(this.x, this.y, this.id);
+        this.text.attr('fill', 'white');
+        window.text = this.text;
+        start = function() {
+          this.ox = this.attr("cx");
+          this.oy = this.attr("cy");
+          return this.animate({
+            r: 15,
+            opacity: .25
+          }, 200, ">");
+        };
+        up = function() {
+          return this.animate({
+            r: 10,
+            opacity: 1
+          }, 200, ">");
+        };
+        text = this.text;
+        move = function(dx, dy) {
+          this.attr({
+            cx: this.ox + dx,
+            cy: this.oy + dy
+          });
+          return text.attr({
+            x: this.ox + dx,
+            y: this.oy + dy
+          });
+        };
+        return this.circle.drag(move, start, up);
+      };
+
+      return Node;
+
+    })();
+    gps_paper = Raphael('gps_map', 940, 400);
+    gps_map = gps_paper.rect(0, 0, 940, 400);
+    gps_map.attr('fill', '#eee');
+    gps_map.attr('stroke', 'none');
+    gps_map.click(function(e) {
+      var node;
+      node = new Node(gps_paper, target_paper, color);
+      node.draw_both(e.offsetX, e.offsetY);
+      return nodes.push(node);
+    });
+    target_paper = Raphael('target_map', 940, 400);
+    target_map = target_paper.rect(0, 0, 940, 400);
+    target_map.attr('fill', '#eee');
+    target_map.attr('stroke', 'none');
+    return target_map.click(function(e) {
+      var node;
+      node = new Node(gps_paper, target_paper, color);
+      node.draw_both(e.offsetX, e.offsetY);
+      return nodes.push(node);
     });
   });
 
